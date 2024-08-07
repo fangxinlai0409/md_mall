60bc475263543b9a735ad487ff0e7d00ea6c590c
fangxinlai0409 <fxfasdf1234@163.com>
2024-08-07 00:43:55 +0000

    converter set

--- md_mall/apps/users/views.py
+++ md_mall/apps/users/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -8,6 +10,8 @@ from apps.users.models import User
 # check user duplication
 class UsernameCountView(View):
 
-    def get(self, request,username):
+    def get(self, request, username):
+        if not re.match('[a-zA-Z0-9_-]{5,20}', username):
+            return JsonResponse({'code':200,'errmsg':'username dose not meet the requirements'})
         count = User.objects.filter(username=username).count()
         return JsonResponse({'code': 0, count: 'count', 'errmsg': 'ok'})
