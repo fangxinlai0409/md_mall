1f3d4c0ecc3143641c070e70ca925e547c8575db
fangxinlai0409 <fxfasdf1234@163.com>
2024-08-07 10:33:55 +0000

    register set

--- md_mall/apps/users/urls.py
+++ md_mall/apps/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from apps.users.views import UsernameCountView
+from . import views
+
 urlpatterns = [
-    path('usernames/<username:username>/count/', UsernameCountView.as_view()),
+    path('usernames/<username:username>/count/', views.UsernameCountView.as_view()),
+    path('register/', views.RegisterView.as_view() ),
 ]
--- md_mall/apps/users/views.py
+++ md_mall/apps/users/views.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from django.http import JsonResponse
@@ -11,7 +12,43 @@ from apps.users.models import User
 class UsernameCountView(View):
 
     def get(self, request, username):
-        if not re.match('[a-zA-Z0-9_-]{5,20}', username):
-            return JsonResponse({'code':200,'errmsg':'username dose not meet the requirements'})
+
         count = User.objects.filter(username=username).count()
-        return JsonResponse({'code': 0, count: 'count', 'errmsg': 'ok'})
+        return JsonResponse({'code': 0, 'count': count, 'errmsg': 'ok'})
+
+
+class RegisterView(View):
+
+    def post(self, request):
+        body_bytes = request.body
+        body_str = body_bytes.decode()
+        body_dict = json.loads(body_str)
+
+        username = body_dict.get('username')
+        password = body_dict.get('password')
+        password2 = body_dict.get('password2')
+        mobile = body_dict.get('mobile')
+        allow = body_dict.get('allow')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return JsonResponse({'code': 400, 'errmsg': '缺少必传参数!'})
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
+            return JsonResponse({'code': 400, 'errmsg': 'username格式有误!'})
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400, 'errmsg': 'password格式有误!'})
+        # 判断两次密码是否一致
+        if password != password2:
+            return JsonResponse({'code': 400, 'errmsg': '两次输入不对!'})
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400, 'errmsg': 'mobile格式有误!'})
+        # 判断是否勾选用户协议
+        if allow != True:
+            return JsonResponse({'code': 400, 'errmsg': 'allow格式有误!'})
+
+        user=User.objects.create_user(username=username, password=password, mobile=mobile)
+        user.save()
+        return JsonResponse({'code': 0, 'errmsg': 'ok'})
