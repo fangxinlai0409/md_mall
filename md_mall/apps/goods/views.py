50e43118ef51e79f465ba25b47c6b2f957777bf8
fangxinlai0409 <fxfasdf1234@163.com>
2024-08-30 16:31:24 +0000

    utils.goods added

--- md_mall/apps/goods/views.py
+++ md_mall/apps/goods/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from fdfs_client.client import Fdfs_client
+
+client = Fdfs_client('/home/fxl/Desktop/meiduoshangcheng/md_mall/md_mall/utils/fastdfs/client.conf')
+print(client.upload_by_filename('/home/fxl/Desktop/Untitled.jpeg'))
---
+++ md_mall/script/import_goos_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -ufxl -pFxf270601! md_mall < goods_data.sql
