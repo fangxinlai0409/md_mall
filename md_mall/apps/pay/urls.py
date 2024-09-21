3a9534f01e7ea1fe1e1bf72926d53ebd0950d9c3
fangxinlai0409 <fxfasdf1234@163.com>
2024-09-21 11:37:57 +0000

    alipay payment status

--- md_mall/apps/pay/urls.py
+++ md_mall/apps/pay/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('payment/<order_id>/',views.PayUrlView.as_view())
+    path('payment/<order_id>/',views.PayUrlView.as_view()),
+    path('/payment/status/',views.PaymentStatusView.as_view()),
 ]
--- md_mall/apps/pay/views.py
+++ md_mall/apps/pay/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from apps.order.models import OrderInfo
+from apps.pay.models import Payment
 from utils.views import LoginRequiredJSONMixin
 from md_mall import settings
 # Create your views here.
@@ -35,3 +36,48 @@ class PayUrlView(LoginRequiredJSONMixin,View):
 
         pay_url = settings.ALIPAY_URL + "?" + order_string
         return JsonResponse({'code': 0, 'errmsg': 'OK', 'alipay_url': pay_url})
+
+class PaymentStatusView(View):
+    """保存订单支付结果"""
+
+    def put(self, request):
+        # 获取前端传入的请求参数
+        query_dict = request.GET
+        data = query_dict.dict()
+        # 获取并从请求参数中剔除signature
+        signature = data.pop('sign')
+        app_private_key_string = open(settings.APP_PRIVATE_KEY_PATH).read()
+        alipay_public_key_string = open(settings.ALIPAY_PUBLIC_KEY_PATH).read()
+        # 创建支付宝支付对象
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_string=app_private_key_string,
+            # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            alipay_public_key_string=alipay_public_key_string,
+            config=AliPayConfig(timeout=15),
+            sign_type="RSA2",
+            debug=settings.ALIPAY_DEBUG
+        )
+        # 校验这个重定向是否是alipay重定向过来的
+        success = alipay.verify(data, signature)
+        if success:
+            # 读取order_id
+            order_id = data.get('out_trade_no')
+            # 读取支付宝流水号
+            trade_id = data.get('trade_no')
+            # 保存Payment模型类数据
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_id
+            )
+
+            # 修改订单状态为待评价
+            OrderInfo.objects.filter(order_id=order_id, status=OrderInfo.ORDER_STATUS_ENUM['UNPAID']).update(
+                status=OrderInfo.ORDER_STATUS_ENUM["UNSEND"])
+
+            # 响应trade_id
+            return JsonResponse({'code': 0, 'errmsg': 'OK', 'trade_id': trade_id})
+        else:
+            # 订单支付失败，重定向到我的订单
+            return JsonResponse({'code': 400, 'errmsg': '非法请求'})
